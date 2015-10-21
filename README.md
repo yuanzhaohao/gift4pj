6c39de85c6b941057c80ef1373493e2bb0e88604
zhaohao.yzh <zhaohao.yzh@alibaba-inc.com>
2015-10-21 21:45:08 +0800

    rebuild

---
+++ README.md
@@ -0,0 +1,16 @@
+Gift4pj
+=====
+> A birthday gift for Pan Jing
+
+## Demo & Examples
+
+Live demo: [http://yuanzhaohao.github.io/gift4pj/](http://yuanzhaohao.github.io/gift4pj/)
+
+To build the examples locally, run:
+
+```
+npm install
+gulp server
+```
+
+Then your example([localhost:3000/](localhost:3000/)) will open automatically in browser.
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var code = require('gulp-code');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var del = require('del');
+
+gulp.task('clean', function (cb) {
+    del(['build'], cb);
+});
+
+gulp.task('js', ['clean'], function () {
+  return gulp.src('src/**/*.js')
+    .pipe(code.lint())
+    .pipe(code.minify())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('less', function () {
+  return gulp.src(['src/**/*.less'])
+    .pipe(less())
+    .pipe(code.lint())
+    .pipe(gulp.dest('src'));
+});
+gulp.task('css', ['clean', 'less'], function () {
+  return gulp.src(['src/**/*.css'])
+    .pipe(code.lint())
+    .pipe(code.minify())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('connect', function () {
+  return connect.server({
+    root: ['./'],
+    livereload: true,
+    port: '3000'
+  });
+});
+gulp.task('open', function () {
+  return gulp.src('./index.html').pipe(open('', { url: 'http://localhost:3000/'}));
+});
+
+gulp.task('default', ['js']);
+gulp.task('server', ['connect', 'open']);

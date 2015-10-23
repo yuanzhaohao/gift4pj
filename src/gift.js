98cdf95c36719c6cc682cd1f0ddb975437348ce8
yuanzhaohao <yuanzhaohao@163.com>
2015-10-23 20:55:53 +0800

    调整样式

--- src/gift.js
+++ src/gift.js
@@ -1,59 +1,79 @@
 (function ($, win, doc) {
 
 $(function () {
-  var rAF = win.requestAnimationFrame || win.webkitRequestAnimationFrame || function (cb) { win.setTimeout(cb, 16); },
-    $gift = $('#J_gift'),
-    offsetX = $gift.width() / 2,
-    offsetY = $gift.height() / 2 - 25,
-    $canvas = $('#J_canvas'),
-    canvas = $canvas[0],
-    ctx;
+  var rAF = win.requestAnimationFrame || win.webkitRequestAnimationFrame || function (cb) { win.setTimeout(cb, 16); };
 
-  canvas.width = $gift.width();
-  canvas.height = $gift.height();
-  ctx = canvas.getContext('2d');
-  ctx.globalCompositeOperation = 'lighter';
-  garden = new Garden(ctx, canvas);
+  function Gift () {
+    return this instanceof Gift
+      ? this.init.call(this, arguments)
+      : new Gift(arguments);
+  }
 
-  setInterval(function () {
-    garden.render();
-  }, Garden.options.growSpeed);
+  Gift.prototype = {
+    contructor: Gift,
 
-  var getHeartPoint = function (angle) {
-  	var t = angle / Math.PI,
-      x = 10.5 * (16 * Math.pow(Math.sin(t), 3)),
-      y = - 10 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
-  	return [offsetX + x, offsetY + y];
-  };
-  var startHeartAnimation = function () {
-  	var interval = 50,
-      angle = 10,
-      heart = [];
-  	var animationTimer = setInterval(function () {
-  		var bloom = getHeartPoint(angle),
-        draw = true;
-  		for (var i = 0; i < heart.length; i++) {
-  			var p = heart[i];
-  			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
-  			if (distance < Garden.options.bloomRadius.max * 1.3) {
-  				draw = false;
-  				break;
-  			}
-  		}
-  		if (draw) {
-  			heart.push(bloom);
-  			garden.createRandomBloom(bloom[0], bloom[1]);
-  		}
-  		if (angle >= 30) {
-  			clearInterval(animationTimer);
-  		}
-      else {
-  			angle += 0.2;
-  		}
-  	}, interval);
-  };
+    init: function () {
+      var self = this;
 
-  startHeartAnimation();
+      self.heart();
+    },
+
+    heart: function () {
+      var self = this,
+        $heart = $('#J_heart'),
+        offsetX = $heart.width() / 2,
+        offsetY = $heart.height() / 2 - 25,
+        $canvas = $('#J_canvas'),
+        canvasEl = $canvas[0],
+        getHeartPoint, heartAnim,
+        ctx, garden;
+
+      canvasEl.width = $heart.width();
+      canvasEl.height = $heart.height();
+      ctx = canvasEl.getContext('2d');
+      ctx.globalCompositeOperation = 'lighter';
+      garden = new Garden(ctx, canvasEl);
+
+      setInterval(function () {
+        garden.render();
+      }, Garden.options.growSpeed);
+      getHeartPoint = function (angle) {
+      	var t = angle / Math.PI,
+          x = 10.6 * (16 * Math.pow(Math.sin(t), 3)),
+          y = - 10 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+      	return [offsetX + x, offsetY + y];
+      };
+      heartAnim = function () {
+      	var interval = 50,
+          angle = 10,
+          heart = [];
+      	var animationTimer = setInterval(function () {
+      		var bloom = getHeartPoint(angle),
+            draw = true;
+      		for (var i = 0; i < heart.length; i++) {
+      			var p = heart[i];
+      			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+      			if (distance < Garden.options.bloomRadius.max * 1.3) {
+      				draw = false;
+      				break;
+      			}
+      		}
+      		if (draw) {
+      			heart.push(bloom);
+      			garden.createRandomBloom(bloom[0], bloom[1]);
+      		}
+      		if (angle >= 30) {
+      			clearInterval(animationTimer);
+      		}
+          else {
+      			angle += 0.2;
+      		}
+      	}, interval);
+      };
+      heartAnim();
+    }
+  }
+  new Gift();
 });
 
 })(Zepto, window, document, undefined);

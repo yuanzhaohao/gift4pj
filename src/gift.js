dd2861af46889c1fe4446bdba22347fe39d39ef7
yuanzhaohao <yuanzhaohao@163.com>
2015-11-08 13:53:34 +0800

    update

--- src/gift.js
+++ src/gift.js
@@ -212,39 +212,39 @@ $(function () {
         garden = new Garden(ctx, canvasEl);
       }
       getHeartPoint = function (angle) {
-      	var t = angle / Math.PI,
+        var t = angle / Math.PI,
           standard = window.innerWidth / 375,
           x = standard * 10.6 * (16 * Math.pow(Math.sin(t), 3)),
           y = - standard * 10 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
-      	return [offsetX + x, offsetY + y];
+        return [offsetX + x, offsetY + y];
       };
       heartAnim = function () {
-      	var interval = 50,
+        var interval = 50,
           angle = 10,
           heart = [];
-      	var animationTimer = setInterval(function () {
-      		var bloom = getHeartPoint(angle),
+        var animationTimer = setInterval(function () {
+          var bloom = getHeartPoint(angle),
             draw = true;
-      		for (var i = 0; i < heart.length; i++) {
-      			var p = heart[i];
-      			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
-      			if (distance < Garden.options.bloomRadius.max * 1.3) {
-      				draw = false;
-      				break;
-      			}
-      		}
-      		if (draw) {
-      			heart.push(bloom);
-      			garden.createRandomBloom(bloom[0], bloom[1]);
-      		}
-      		if (angle >= 30) {
-      			clearInterval(animationTimer);
+          for (var i = 0; i < heart.length; i++) {
+            var p = heart[i];
+            var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+            if (distance < Garden.options.bloomRadius.max * 1.3) {
+              draw = false;
+              break;
+            }
+          }
+          if (draw) {
+            heart.push(bloom);
+            garden.createRandomBloom(bloom[0], bloom[1]);
+          }
+          if (angle >= 30) {
+            clearInterval(animationTimer);
             self.time();
-      		}
+          }
           else {
-      			angle += 0.2;
-      		}
-      	}, interval);
+            angle += 0.2;
+          }
+        }, interval);
       };
       resize();
       heartAnim();
@@ -286,7 +286,7 @@ $(function () {
           $count.html(time);
         }
       );
-      updateFn && win.addEventListener('scroll', utils.buffer(updateFn, 500), true);
+      // updateFn && win.addEventListener('scroll', utils.buffer(updateFn, 500), true);
     },
 
     time: function () {
